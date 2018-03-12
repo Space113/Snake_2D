83c872c989165ba509a3c0a507b7632916bc8787
Konstantin <kostya.ryabocon@gmail.com>
2018-03-12 11:53:42 +0200

    Add snake controll and movement
    - change snake asset;
    - edit player and background size;
    - add pickup asset to scene

---
+++ Snake_2D/Assets/Scripts/GameController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject snakePrefab;
+	public Snake head;
+	public Snake tail;
+
+	private Rigidbody2D rb2d; 
+	// Current Movement Direction
+	// (by default it moves to the right)
+	Vector2 dir = Vector2.up;
+
+	void Start () {
+//		rb2d = GetComponent<Rigidbody2D> ();
+
+
+		// Move the Snake every 300ms
+		InvokeRepeating("Move", 0.1f, 0.1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Move in a new Direction?
+		if (Input.GetKey(KeyCode.RightArrow))
+			dir = Vector2.right;
+		else if (Input.GetKey(KeyCode.DownArrow))
+			dir = -Vector2.up;    // '-up' means 'down'
+		else if (Input.GetKey(KeyCode.LeftArrow))
+			dir = -Vector2.right; // '-right' means 'left'
+		else if (Input.GetKey(KeyCode.UpArrow))
+			dir = Vector2.up;
+	}
+
+	void Move() {
+		transform.Translate (dir);
+	}
+}
