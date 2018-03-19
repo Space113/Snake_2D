1f739b60ef9523bdab58c8ab7b582a296b724b4a
Konstantin <kostya.ryabocon@gmail.com>
2018-03-20 01:33:56 +0200

    Feading the snake
    - Fixed pickup prefab
    - Add collectible objects
    - adding new segment of snake every time while it`s eating
    - remove unusable assets

--- Snake_2D/Assets/Scripts/GameController.cs
+++ Snake_2D/Assets/Scripts/GameController.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GameController : MonoBehaviour {
 
-	public GameObject snakePrefab;
-	public Snake head;
-	public Snake tail;
+	public GameObject tailPrefab;
+//	public Snake head;
+	// Keep Track of Tail
+
+	private bool ate = false;
+	private List<Transform> tail = new List<Transform>();
 
 	private Rigidbody2D rb2d; 
 	// Current Movement Direction
@@ -35,6 +39,47 @@ public class GameController : MonoBehaviour {
 	}
 
 	void Move() {
+		// Save current position (gap will be here)
+		Vector2 v = transform.position;
+
 		transform.Translate (dir);
+		// Ate something? Then insert new Element into gap
+		if (ate) {
+			// Load Prefab into the world
+			GameObject g =(GameObject)Instantiate(tailPrefab, v, transform.rotation);
+
+			// Keep track of it in our tail list
+			tail.Insert(0, g.transform);
+
+			// Reset the flag
+			ate = false;
+		}
+		// Do we have a Tail?
+		else if (tail.Count > 0) {
+			// Move last Tail Element to where the Head was
+			tail.Last().position = v;
+
+			// Add to front of list, remove from the back
+			tail.Insert(0, tail.Last());
+			tail.RemoveAt(tail.Count-1);
+		}
+
+	}
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		// Food?
+		if (coll.gameObject.CompareTag ("PickUp")) {
+			// Get longer in next Move call
+			ate = true;
+
+			// Remove the Food
+			coll.gameObject.SetActive (false);
+//			Destroy(coll.gameObject);
+
+		}
+		// Collided with Tail or Border
+		else {
+			// ToDo 'You lose' screen
+		}
 	}
 }
